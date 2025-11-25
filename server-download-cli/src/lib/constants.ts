baa1b413a3002a7c221a192903c2b96f3fc00b2b
VMD6002 <vmdchelembra@gmail.com>
2025-11-25 12:16:42 +0530

    Updated user agent

--- server-download-cli/src/lib/constants.ts
+++ server-download-cli/src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const TMP_DIR = "./tmp";
 export const COMPLETED_DIR = "../Sync";
 export const USER_AGENT =
-  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.107 Safari/537.36";
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36";
